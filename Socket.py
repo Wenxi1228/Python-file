6ba0d61ce1e1a6c7f8efac4a7ffb1c56be3b415d
Wenxi1228 <brucefu527@126.com>
2022-04-21 18:05:43 +0800

    bind port initial version

--- Socket.py
+++ Socket.py
@@ -2,13 +2,19 @@
 import socket
 import time
 import datetime
+import threading
 # 创建socket对象
-while 1:
+def socket_tcp(ip_addr,send_port):
     s = socket.socket()
     # 连接远程主机
-    s.connect(('172.31.3.67', 30517))
+    s.connect((ip_addr, send_port))
     #print('--%s--' % s.recv(1024).decode('utf-8'))
     print(datetime.datetime.now())
     time.sleep(5)
     s.close()
     time.sleep(5)
+
+
+if __name__=='__main__':
+    while 1:
+        socket_tcp('172.31.3.67',30517)
