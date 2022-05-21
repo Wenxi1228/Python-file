4c905d9a877b5ff8c1228e6632e15ee016750e9b
Wenxi1228 <brucefu527@126.com>
2022-05-21 16:58:52 +0800

    update more cases

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from numpy import float32, uint8
-
+from numpy import float32, uint8, uint16, uint32
 
 a=-10.001
 b= uint8(a)
-print(a,type(a),b,type(b))
+c= uint16(a*2.0+34)
+d= uint32(c)
+print(a,type(a),b,type(b),c,type(c),d,type(d))
